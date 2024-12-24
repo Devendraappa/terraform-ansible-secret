f1744c4f08f8307f2faa5f1acea303ec237759c4
Devendraappa <devendraappa.kune@gmail.com>
2024-12-24 11:04:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,11 +52,3 @@ resource "aws_instance" "web_server" {
   }
 }
 
-output "instance_public_ip" {
-  value = aws_instance.web_server.public_ip
-}
-
-output "private_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
-}
