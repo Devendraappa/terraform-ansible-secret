ab6791c3102c7d3f360463ac1026990b68588985
Devendraappa <devendraappa.kune@gmail.com>
2024-12-24 11:06:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,7 @@ output "instance_public_ip" {
   value = aws_instance.web_server.public_ip
 }
 
-output "instance_id" {
-  value = aws_instance.web_server.id
+output "private_key" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
 }
